527f3727961eacd804553c67ce2d40f7ae792bbc
TrevorPage <trevg60@gmail.com>
2025-05-28 22:40:25 +0100

    Add optional start and extent angle support to Circle

--- packages/draw/src/Circle.ts
+++ packages/draw/src/Circle.ts
@@ -10,6 +10,8 @@ interface CircleOptions {
   y: number;
   radius: number;
   color: Color;
+  extentAngle?: number;
+  startAngle?: number;
 }
 
 export class Circle extends Shape {
@@ -17,6 +19,8 @@ export class Circle extends Shape {
   y: number;
   radius: number;
   color: Color;
+  extentAngle?: number;
+  startAngle?: number;  
 
   constructor(options: CircleOptions) {
     super();
@@ -24,15 +28,19 @@ export class Circle extends Shape {
     this.y = options.y;
     this.radius = options.radius;
     this.color = options.color;
+    this.extentAngle = options.extentAngle;
+    this.startAngle = options.startAngle;
   }
 
   draw(options: SceneOptions): Point[] {
     const circumference = 2.0 * this.radius * Math.PI;
     const pointCount = Math.round(circumference * options.resolution);
+    const extentRadians = this.extentAngle ? (this.extentAngle * Math.PI) / 180.0 : Math.PI * 2;
+    const startRadians = this.startAngle ? (this.startAngle * Math.PI) / 180.0 : 0;
 
     const points: Point[] = new Wait({
-      x: this.x + this.radius,
-      y: this.y,
+      x: this.x - this.radius * Math.cos(startRadians),
+      y: this.y + this.radius * Math.sin(startRadians),
       color: [0, 0, 0],
       amount: options.blankingPoints
     }).draw();
@@ -43,10 +51,10 @@ export class Circle extends Shape {
     }
 
     const stepSize = (Math.PI * 2) / pointCount;
-    for (let i: number = 0.0; i < Math.PI * 2; i += stepSize) {
+    for (let i: number = startRadians; i < (extentRadians + startRadians); i += stepSize) {
       points.push(
         new Point(
-          this.x + this.radius * Math.cos(i),
+          this.x - this.radius * Math.cos(i),
           this.y + this.radius * Math.sin(i),
           this.color
         )
@@ -54,13 +62,20 @@ export class Circle extends Shape {
     }
 
     // Close circle
-    points.push(new Point(this.x + this.radius, this.y, this.color));
+    points.push(
+      new Point(
+        this.x - this.radius * Math.cos(extentRadians + startRadians),
+        this.y + this.radius * Math.sin(extentRadians + startRadians),
+        this.color)
+    );
+
+    //console.log("blanking point is at x:", this.x + this.radius, "y:", this.y);
 
     // Blank after
     return points.concat(
       new Wait({
-        x: this.x + this.radius,
-        y: this.y,
+        x: this.x - this.radius * Math.cos(extentRadians + startRadians),
+        y: this.y + this.radius * Math.sin(extentRadians + startRadians),
         color: this.color,
         amount: options.maxWaitPoints / 2
       }).draw()
